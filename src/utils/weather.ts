b48d471a0101590be84570fa7c427b45fcfd516c
tascript <wataruscript8611@gmail.com>
2023-06-13 01:30:07 +0900

    delete extra code

--- src/utils/weather.ts
+++ src/utils/weather.ts
@@ -11,7 +11,7 @@ export interface WeatherResult {
 }
 
 export async function weather() {
-  const res = await fetch('https://api.open-meteo.com/v1/forecast?latitude=33.60&longitude=130.42&hourly=temperature_2m,rain&forecast_days=3')
+  const res = await fetch('https://api.open-meteo.com/v1/forecast?latitude=33.60&longitude=130.42&hourly=temperature_2m,rain&forecast_days=1&timezone=Asia%2FTokyo')
   const report = await res.json() as WeatherRawData
   const temperature = report.hourly.temperature_2m
   const time = report.hourly.time
