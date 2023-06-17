e7cd8478a2de4e2a51768ff85443e5c6885fb036
tascript <wataruscript8611@gmail.com>
2023-06-17 20:55:03 +0900

    use css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import '~/assets/css/App.css'
+import styles from '~/assets/css/App.module.css'
+import Chart from './Chart.tsx'
 
 function App() {
 
   return (
     <>
       <h1>hryc</h1>
-      <div className="card">
+      <div className={styles.card}>
         <p>
           hryc is daily weather report in Fukuoka City.
         </p>
       </div>
+      <Chart />
     </>
   )
 }
