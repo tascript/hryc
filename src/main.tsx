52755315965034192d431d2e5a7822d5fcab5c5f
tascript <wataruscript8611@gmail.com>
2023-06-17 19:38:24 +0900

    set path alias

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import Chart from './Chart.tsx'
-import './index.css'
+import '~/assets/css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
