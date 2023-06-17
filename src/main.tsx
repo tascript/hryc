20a672ca87c12a07cc01050c3fcf176ab9c8da10
tascript <wataruscript8611@gmail.com>
2023-06-17 19:06:50 +0900

    fix typo

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import Chart from './chart.tsx'
+import Chart from './Chart.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
